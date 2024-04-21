19d135533a1085600e390a4a669abd629bff2e21
amanu9 <106382997+amanu9@users.noreply.github.com>
2024-04-21 11:24:36 +0300

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+// index.js
+Page({
+  data: {
+    motto: 'test World',
+    
+    
+  },
+  
+  onLoad(options) {},
+  onReady() {},
+  onShow() {},
+  onHide() {},
+  onUnload() {},
+  onShareAppMessage() {
+    return {
+      title: 'tewos'
+    };
+  }
+});
