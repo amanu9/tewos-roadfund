ce72fee25fd2945c471ce90936c7d755993f36c2
amanu9 <106382997+amanu9@users.noreply.github.com>
2024-04-22 08:09:57 +0300

    some update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,8 @@
-// index.js
+
+
 Page({
   data: {
-    motto: 'test World',
+   
     
     
   },
@@ -15,5 +16,50 @@ Page({
     return {
       title: 'tewos'
     };
+  },
+
+  navigateToform:function(){
+    ma.navigateTo({
+      url:'/pages/form/form'
+    })
+  },
+  validateInput: function(event) {
+    // Get the input value
+    const inputValue = event.detail.value.input;
+
+    // Check  input is empty 
+    if (!inputValue) {
+      // Prevent the form from being submitted
+      ma.showModal({
+        title: 'Please insert a Payment Reference No.',
+        content: '', 
+        showCancel: true, 
+        confirmButtonText: 'Cancel', 
+        cancelButtonText: 'OK', 
+       });
+    
+      return false;
+    }
+    // add more
+    return true;
+ },
+
+ // form submission handle
+formSubmit: function(event) {
+
+  if (!this.validateInput(event)) {
+    return; 
   }
+
+  //or success 
+  this.navigateToform();
+ }, 
+
 });
+
+
+
+
+
+
+
